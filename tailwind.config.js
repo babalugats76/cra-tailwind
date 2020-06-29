ab969f31d830c258a13667bfb76bab400acfc760
James Colestock <james@colestock.com>
2020-06-28 18:03:54 -0600

    Additional tailwind extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
         // autosize grids
         "auto-min-w-180": "repeat(auto-fill, minmax(180px, 1fr))",
       },
-    
+      inset: {
+        // add 50% as an option to positioning classes
+        '1/2': '50%',
+      },
+      translate: {
+        '-1/2': '-50%'
+      }
     },
   },
   plugins: [],
