6a9b4928f92dca95f17fae714dc1ca1b90398b4b
James Colestock <james@colestock.com>
2020-07-04 15:17:43 -0600

    More tailwind config; first trial application using apply, etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {      
+    extend: {    
+      fontFamily : {
+        'hind': '"Hind", sans-serif',
+        'lexend-deca': '"Lexend Deca", sans-serif',
+      }, 
+      fontSize: {
+        'md': '0.9375rem'
+      },
       gridTemplateColumns: {
         // autosize grids
         "auto-min-w-180": "repeat(auto-fill, minmax(180px, 1fr))",
@@ -9,6 +16,9 @@ module.exports = {
       inset: {
         // add 50% as an option to positioning classes
         "1/2": "50%",
+      },
+      lineHeight: {
+        'extra-tight': '1.125',
       },
       spacing: {
         "7": "1.75rem",
